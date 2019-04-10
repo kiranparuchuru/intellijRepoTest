ee14edbe146cc37567fe097b0f6256c38f63a6ed
Kiran Paruchuru <kiran.paruchuru@vancopayments.com>
2019-04-10 15:57:09 -0400

    libraries added and testng class

---
+++ demo/src/main/java/TestNGTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGTest {
+
+
+    @BeforeMethod
+    public void beforeTestRuns() {
+        System.out.println("before test");
+    }
+
+
+
+    @Test
+    public void firstTest() {
+        System.out.println("inside test");
+    }
+
+
+}
